853d396c42a6f62b81106f2ad0c6250c5e8b256e
Nurbolot <ngulamidinov@gmail.com>
2023-01-15 19:00:25 +0600

    add integration test CompanyServiceIT

--- src/test/java/org/ntg/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/org/ntg/spring/integration/service/CompanyServiceIT.java
@@ -1,23 +1,26 @@
 package org.ntg.spring.integration.service;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.ntg.spring.config.DatabaseProperties;
 import org.ntg.spring.dto.CompanyReadDto;
+import org.ntg.spring.integration.annotation.IntegrationTest;
 import org.ntg.spring.service.CompanyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-@SpringBootTest
-@ActiveProfiles("test")
-public class companyServiceIT {
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = SpringApplicationRunner.class,
+// initializers = ConfigDataApplicationContextInitializer.class)
+
+@IntegrationTest
+@RequiredArgsConstructor
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+public class CompanyServiceIT {
     private static final int COMPANY_ID = 1;
-    @Autowired
-    private CompanyService companyService;
-    @Autowired
-    private DatabaseProperties databaseProperties;
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
